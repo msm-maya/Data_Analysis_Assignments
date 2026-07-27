300b2b25ddf823cd7288c9bf198c374f0dfd51ec
msm-maya <mayasardar2222@gmail.com>
2026-07-27 23:00:42 +0600

    add lab report

--- assignment_1.py
+++ assignment_1.py
@@ -2,19 +2,15 @@ import pandas as pd
 
 # 1. Load CSV dataset
 df = pd.read_csv("Space_Corrected.csv")
-
 # 2. Display first 5 rows
 print("--- First 5 Rows ---")
 print(df.head())
-
 # 3. Show column names
 print("\n--- Column Names ---")
 print(df.columns.tolist())
-
 # 4. Display number of rows and columns
 print("\n--- Number of Rows and Columns ---")
 print(f"Rows: {df.shape[0]}, Columns: {df.shape[1]}")
-
 # 5. Show summary statistics
 print("\n--- Summary Statistics ---")
 print(df.describe())
